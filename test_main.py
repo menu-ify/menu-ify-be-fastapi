2425c8433d86f56287199c7b6fa133f47ac2457c
Emily Port <emilylport@gmail.com>
2023-02-12 14:09:16 -0700

    update circle config file

---
+++ test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient 
+
+from main import app 
+
+client = TestClient(app)
+
+def test_main(): 
+  response = client.get("/photos/cheese")
+  assert response.status_code == 200 
