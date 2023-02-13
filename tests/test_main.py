c77876dec3676e4c5091e805037dde852c88d09f
Emily Port <emilylport@gmail.com>
2023-02-13 12:13:48 -0700

    refactored test file to import app from main

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,27 +1,5 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
-import pandas as pd 
-import requests 
-from dotenv import load_dotenv 
-import os
-
-def configure():
-  load_dotenv() 
-
-
-app = FastAPI()
-
-@app.get("/photos/{keyword}")
-def get_photos(keyword: str):
-  configure()
-  url = f"https://api.unsplash.com/search/photos?page=1&query=" + keyword + "&client_id=" + os.getenv("api_key")
-  response = requests.get(url)
-  new = response.json()
-  new_results = new["results"]
-  photo_array = []
-  for item in new_results:
-    photo_array.append((item["urls"]["raw"]))
-  return {"results": photo_array }
+from main import app 
 
 client = TestClient(app)
 
