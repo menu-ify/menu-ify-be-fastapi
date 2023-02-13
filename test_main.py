de4ee6e3bc652ebb569199958c8a5bec99989079
Emily Port <emilylport@gmail.com>
2023-02-13 11:06:34 -0700

    update

--- test_main.py
+++ test_main.py
@@ -1,16 +1,11 @@
 from fastapi.testclient import TestClient 
-from dotenv import load_dotenv 
-import os
-
 from main import app  
 
-def configure():
-  load_dotenv() 
+
 
 client = TestClient(app)
 
 
 def test_main(keyword = 'cheese'): 
-  configure()
   response = client.get("/photos/{keyword}")
   assert response.status_code == 200 
