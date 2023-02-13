ffccd1419641931f2fe67a3b15d9db88881376a8
Emily Port <emilylport@gmail.com>
2023-02-13 10:48:54 -0700

    update test

--- test_main.py
+++ test_main.py
@@ -4,6 +4,7 @@ from main import app
 
 client = TestClient(app)
 
-def test_main(keyword = "cheese"): 
+
+def test_main(keyword = 'cheese'): 
   response = client.get("/photos/{keyword}")
   assert response.status_code == 200 
