eae5656a9c944fb83d6c1b7f4c299581ceefcbbd
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-02-14 15:53:34 -0700

    Add vcr to testing to mock api calls

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,16 @@
 from fastapi.testclient import TestClient
+import vcr
+import urllib3
 from main import app 
 
 client = TestClient(app)
 
+@vcr.use_cassette('tests/fixtures/vcr_cassettes/unsplash_bacon.yaml', filter_query_parameters=['client_id'])
 def test_read_main():
     response = client.get("/photos/bacon")
     assert response.status_code == 200
 
+@vcr.use_cassette('tests/fixtures/vcr_cassettes/unsplash_bacon.yaml', filter_query_parameters=['client_id'])
 def test_read_main_response():
     response = client.get("/photos/bacon")
     assert "results" in response.json()
