ed9f806804912b803115a25d236aeea53dd73070
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-02-14 16:55:12 -0700

    Refactor tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,14 +3,12 @@ from main import app
 
 client = TestClient(app)
 
-def test_read_main():
-    response = client.get("/photos/bacon")
-    assert response.status_code == 200
-
 def test_read_main_response():
     response = client.get("/photos/bacon")
+    assert response.status_code == 200
     assert "results" in response.json()
 
 def test_no_photos_found():
     response = client.get("/photos/asdfjklqweruiop")
-    assert "message" in response.json()
+    assert response.status_code == 200
+    assert response.json() == {"message": "No photos found"}
