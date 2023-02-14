66afcfc447022bd71be4df0f4dde3dabd6fcb5f9
Gabe Nunez <gabriel.c.nunez@gmail.com>
2023-02-14 16:38:58 -0700

    Add conditional for no photos found, pass testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,12 +5,16 @@ from main import app
 
 client = TestClient(app)
 
-@vcr.use_cassette('tests/fixtures/vcr_cassettes/unsplash_bacon.yaml', filter_query_parameters=['client_id'])
+# @vcr.use_cassette('tests/fixtures/vcr_cassettes/unsplash_bacon.yaml', filter_query_parameters=['client_id'])
 def test_read_main():
     response = client.get("/photos/bacon")
     assert response.status_code == 200
 
-@vcr.use_cassette('tests/fixtures/vcr_cassettes/unsplash_bacon.yaml', filter_query_parameters=['client_id'])
+# @vcr.use_cassette('tests/fixtures/vcr_cassettes/unsplash_bacon.yaml', filter_query_parameters=['client_id'])
 def test_read_main_response():
     response = client.get("/photos/bacon")
     assert "results" in response.json()
+
+def test_no_photos_found():
+    response = client.get("/photos/asdfjklqweruiop")
+    assert "message" in response.json()
